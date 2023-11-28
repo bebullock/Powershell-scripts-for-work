6efa8865ece13d95b10986d41588cff52aec52cc
Patrick Bullock-Korotney <58739113+bebullock@users.noreply.github.com>
2023-11-28 09:23:40 -0500

    RMM agent auto curl and install
    This one will have to be tailored to needed RMM agents or other programs that can be installed without having to go through a setup menu. can copy lines 9-13 and use again if more than two programs are needed

---
+++ RMM agent curl and auto install.ps1
@@ -0,0 +1,13 @@
+﻿Read-Host -Promt "Please ensure no other installers are running, if this is true press enter to continue"
+
+curl "#Instert URL of needed RMM agent here#" -o c:\windows\temp\ #Name the file here remebering to add required file extention
+
+#The above is the URL and the path to where the syncro installer is located, if you run into any issues
+
+c:\windows\temp\ #input file name you used on line 3 after the output command --console --customerid  --folderid # <--- this is a Syncro specific line, if not using Sycnro everything after the file extention can be removed on this line
+
+Read-Host -Prompt "Press enter to continue once Syncro install has completed"
+
+curl "#Insert URL of needed RMM agent here" -o C:\windows\temp\ #Name the file here remebering to add required file extention
+
+C:\windows\temp\ #Name the file here remebering to add required file extention from line 11
